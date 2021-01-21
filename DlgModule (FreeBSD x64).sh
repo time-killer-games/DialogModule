6576c7a54e941e791b131530c83268eead62405e
time-killer-games <samuelvenable@hotmail.com>
2021-01-21 20:21:07 +0000

    Add files via upload

--- DlgModule (FreeBSD x64).sh
+++ DlgModule (FreeBSD x64).sh
@@ -2,4 +2,4 @@ cd "${0%/*}"
 
 mkdir "DlgModule (x64)"
 mkdir "DlgModule (x64)/FreeBSD"
-g++ "DlgModule/dlgmodule.cpp" "DlgModule/xlib/dlgmodule.cpp" "DlgModule/xlib/lodepng/lodepng.cpp" "DlgModule/xlib/ProcInfo/helpers.cpp" "DlgModule/xlib/ProcInfo/FreeBSD/procinfo.cpp" "DlgModule/xlib/ProcInfo/xlib/procinfo.cpp" "DlgModule/xlib/ProcInfo/POSIX/procinfo.cpp" "DlgModule/xlib/ProcInfo/Universal/procinfo.cpp" -o "DlgModule (x64)/FreeBSD/libdlgmod.so" -DPROCINFO_SELF_CONTAINED -std=c++17 -shared -static-libgcc -static-libstdc++ -lX11 -lprocstat -lutil -lc -lpthread -fPIC -m64
+g++ "DlgModule/dlgmodule.cpp" "DlgModule/xlib/dlgmodule.cpp" "DlgModule/xlib/lodepng/lodepng.cpp" -o "DlgModule (x64)/FreeBSD/libdlgmod.so" -std=c++17 -shared -static-libgcc -static-libstdc++ -lX11 -lutil -lpthread -fPIC -m64
--- DlgModule (Linux x64).sh
+++ DlgModule (Linux x64).sh
@@ -2,4 +2,4 @@ cd "${0%/*}"
 
 mkdir "DlgModule (x64)"
 mkdir "DlgModule (x64)/Linux"
-g++ "DlgModule/dlgmodule.cpp" "DlgModule/xlib/dlgmodule.cpp" "DlgModule/xlib/lodepng/lodepng.cpp" "DlgModule/xlib/ProcInfo/helpers.cpp" "DlgModule/xlib/ProcInfo/Linux/procinfo.cpp" "DlgModule/xlib/ProcInfo/xlib/procinfo.cpp" "DlgModule/xlib/ProcInfo/POSIX/procinfo.cpp" "DlgModule/xlib/ProcInfo/Universal/procinfo.cpp" -o "DlgModule (x64)/Linux/libdlgmod.so" -DPROCINFO_SELF_CONTAINED -std=c++17 -shared -static-libgcc -static-libstdc++ -lX11 -lprocps -lpthread -fPIC -m64
+g++ "DlgModule/dlgmodule.cpp" "DlgModule/xlib/dlgmodule.cpp" "DlgModule/xlib/lodepng/lodepng.cpp" -o "DlgModule (x64)/Linux/libdlgmod.so" -std=c++17 -shared -static-libgcc -static-libstdc++ -lX11 -lprocps -lpthread -fPIC -m64
