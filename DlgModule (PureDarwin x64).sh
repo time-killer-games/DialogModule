56ba734e37ba29db0eedbc10ff7b7b4ee8b3658d
time-killer-games <samuelvenable@hotmail.com>
2021-01-24 22:31:21 -0500

    Delete DlgModule (PureDarwin x64).sh

--- DlgModule (PureDarwin x64).sh
+++
@@ -1,6 +0,0 @@
-cd "${0%/*}"
-
-mkdir "DlgModule (x64)"
-mkdir "DlgModule (x64)/PureDarwin"
-export SDKROOT=`xcrun --show-sdk-path`
-/opt/local/bin/g++-mp-* "DlgModule/dlgmodule.cpp" "DlgModule/xlib/dlgmodule.cpp" "DlgModule/xlib/lodepng/lodepng.cpp" -o "DlgModule (x64)/PureDarwin/libdlgmod.dylib" -std=c++17 -shared  -static-libgcc -static-libstdc++ -I/opt/X11/include -L/opt/X11/lib -lX11 -fPIC -m64
