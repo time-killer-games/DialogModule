0259f305edfd21c39454e62a7d58bf2886c9b37b
time-killer-games <samuelvenable@hotmail.com>
2021-01-24 19:25:39 -0500

    Update DlgModule (PureDarwin x64).sh

--- DlgModule (PureDarwin x64).sh
+++ DlgModule (PureDarwin x64).sh
@@ -2,4 +2,4 @@ cd "${0%/*}"
 
 mkdir "DlgModule (x64)"
 mkdir "DlgModule (x64)/PureDarwin"
-clang++ "DlgModule/dlgmodule.cpp" "DlgModule/xlib/dlgmodule.cpp" "DlgModule/xlib/lodepng/lodepng.cpp" -o "DlgModule (x64)/PureDarwin/libdlgmod.dylib" -std=c++17 -shared -lX11 -fPIC -m64
+clang++ "DlgModule/dlgmodule.cpp" "DlgModule/xlib/dlgmodule.cpp" "DlgModule/xlib/lodepng/lodepng.cpp" -o "DlgModule (x64)/PureDarwin/libdlgmod.dylib" -std=c++17 -shared -I/opt/X11/include/X11 -L/opt/X11/lib -lX11 -fPIC -m64
