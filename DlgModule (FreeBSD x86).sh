dd4f894691db81718c155e7f36d4bf6fecdf572e
time-killer-games <samuelvenable@hotmail.com>
2021-01-24 19:15:06 -0500

    Update DlgModule (FreeBSD x86).sh

--- DlgModule (FreeBSD x86).sh
+++ DlgModule (FreeBSD x86).sh
@@ -2,4 +2,4 @@ cd "${0%/*}"
 
 mkdir "DlgModule (x86)"
 mkdir "DlgModule (x86)/FreeBSD"
-g++ "DlgModule/dlgmodule.cpp" "DlgModule/xlib/dlgmodule.cpp" "DlgModule/xlib/lodepng/lodepng.cpp" -o "DlgModule (x86)/FreeBSD/libdlgmod.so" -std=c++17 -shared -static-libgcc -static-libstdc++ -lX11 -lutil -lpthread -fPIC -m32
+clang++ "DlgModule/dlgmodule.cpp" "DlgModule/xlib/dlgmodule.cpp" "DlgModule/xlib/lodepng/lodepng.cpp" -o "DlgModule (x86)/FreeBSD/libdlgmod.so" -std=c++17 -shared -lX11 -lutil -lpthread -fPIC -m32
