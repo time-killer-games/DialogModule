f0a57bf8b7a4f9df33cbb03ace832fe52be10c5f
time-killer-games <samuelvenable@hotmail.com>
2021-01-27 07:57:03 -0500

    Delete DlgModule (Linux x64).sh

--- DlgModule (Linux x64).sh
+++
@@ -1,5 +0,0 @@
-cd "${0%/*}"
-
-mkdir "DlgModule (x64)"
-mkdir "DlgModule (x64)/Linux"
-g++ "DlgModule/dlgmodule.cpp" "DlgModule/xlib/dlgmodule.cpp" "DlgModule/xlib/lodepng/lodepng.cpp" -o "DlgModule (x64)/Linux/libdlgmod.so" -std=c++17 -shared -static-libgcc -static-libstdc++ -lX11 -lprocps -lpthread -fPIC -m64
