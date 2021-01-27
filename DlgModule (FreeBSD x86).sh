14cda3264cb5e1215598aa4070fb8869d87e167e
time-killer-games <samuelvenable@hotmail.com>
2021-01-27 07:57:09 -0500

    Delete DlgModule (FreeBSD x86).sh

--- DlgModule (FreeBSD x86).sh
+++
@@ -1,5 +0,0 @@
-cd "${0%/*}"
-
-mkdir "DlgModule (x86)"
-mkdir "DlgModule (x86)/FreeBSD"
-clang++ "DlgModule/dlgmodule.cpp" "DlgModule/xlib/dlgmodule.cpp" "DlgModule/xlib/lodepng/lodepng.cpp" -o "DlgModule (x86)/FreeBSD/libdlgmod.so" -std=c++17 -shared -lX11 -lutil -lpthread -fPIC -m32
