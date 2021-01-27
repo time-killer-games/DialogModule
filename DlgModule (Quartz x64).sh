7820b19fcbdfe16f62b4c37eda5256afb85edbb9
time-killer-games <samuelvenable@hotmail.com>
2021-01-27 07:56:43 -0500

    Delete DlgModule (Quartz x64).sh

--- DlgModule (Quartz x64).sh
+++
@@ -1,7 +0,0 @@
-cd "${0%/*}"
-
-mkdir "DlgModule (x64)"
-mkdir "DlgModule (x64)/Quartz"
-clang++ -c -ObjC "DlgModule/xlib/quartz/dlgmodule.m" "DlgModule/xlib/quartz/subclass.m" -fPIC -m64
-clang++ "DlgModule/dlgmodule.cpp" "DlgModule/xlib/dlgmodule.cpp" "dlgmodule.o" "subclass.o" -o "DlgModule (x64)/Quartz/libdlgmod.dylib" -std=c++17 -shared -framework Cocoa -fPIC -m64
-rm -f "dlgmodule.o" "subclass.o"
