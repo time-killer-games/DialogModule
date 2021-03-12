c2e03eb51936e5c5e2e83c11ad68b9c90afa8953
time-killer-games <samuelvenable@hotmail.com>
2021-03-11 19:43:07 -0500

    Add files via upload

--- DlgModule (MacOSX x64).sh
+++ DlgModule (MacOSX x64).sh
@@ -2,6 +2,4 @@ cd "${0%/*}"
 
 mkdir "DlgModule (x64)"
 mkdir "DlgModule (x64)/MacOSX"
-clang++ -c -ObjC "DlgModule/MacOSX/Cocoa/dlgmodule.m" -fPIC -m64
-clang++ "DlgModule/dlgmodule.cpp" "DlgModule/MacOSX/dlgmodule.cpp" "DlgModule/MacOSX/config/config.cpp" "dlgmodule.o" -o "DlgModule (x64)/MacOSX/libdlgmod.dylib" -std=c++17 -framework Cocoa -shared -fPIC -m64
-rm -f "dlgmodule.o"
+clang++ "DlgModule/Universal/dlgmodule.cpp" "DlgModule/MacOSX/dlgmodule.mm" "DlgModule/MacOSX/config.cpp" -o "DlgModule (x64)/MacOSX/libdlgmod.dylib" -std=c++17 -ObjC++ -framework Cocoa -shared -fPIC -m64
